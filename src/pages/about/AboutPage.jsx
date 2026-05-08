17bdfef1d9eae684fac6c3a6122e8bfc9860cc63
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2026-05-08 11:39:38 +0200

    Profile photo and text

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -9,18 +9,19 @@ export const AboutPage = () => {
             <Header />
             <main className="Main-aboutContent">
                 <img className="Main-aboutImg" src="/imgs/img-about-me.png" alt="" />
-                <p className="Main-aboutText">¡Hola! Soy Jaime y soy <strong>Desarrollador Front-end Junior</strong> especializado en el <strong>stack MERN</strong> y en <strong>Next.js</strong>. Con dominio de <strong>JavaScript, HTML5 y CSS3</strong>, cuento con experiencia en la creación y desarrollo de páginas web, banners y mails, logrando así productos digitales responsive optimizados en distintos dispositivos que cuentan con código y componentes limpios y escalables.<br/> 
-                
-                Especialmente interesado en la creación de experiencias digitales atractivas, funcionales y orientadas al usuario, mi perfil integra una fuerte sensibilidad por el diseño gráfico y web, permitiéndome transformar prototipos en experiencias reales.<br/>
-                
-                Aspiro a integrarme en un entorno profesional donde seguir fortaleciendo mis conocimientos y contribuir al desarrollo de soluciones tecnológicas innovadoras.</p><br />
+                <p className="Main-aboutText">
+                ¡Hola! Soy Jaime, perfil con experiencia en <strong>tecnología y negocio</strong>, combinando <strong>desarrollo web (stack MERN) y soluciones digitales</strong>.<br />
+                Me caracterizo por un enfoque analítico y orientado a la resolución de problemas. He trabajado directamente con clientes B2B y B2C, entendiendo sus necesidades y proponiendo soluciones digitales adaptadas, lo que me ha dado una visión práctica de cómo la tecnología impacta en procesos y resultados.<br />
+                Interesado en la consultoría digital, aporto una visión técnica combinada con una buena comprensión de las necesidades del cliente y del negocio.</p><br />
                 <div>
                     <p className="Main-aboutTitle">De las ideas al código...</p>
                     <p className="Main-aboutText">Mi camino profesional comenzó en el ámbito de la creatividad y la comunicación. Tras graduarme en Publicidad y Relaciones Públicas, trabajé en un proyecto de agencia de comunicación, donde descubrí el poder de convertir ideas en marcas. Más tarde, en el CEEI de Castellón, me acerqué al ecosistema startup, entendiendo cómo la tecnología y la estrategia digital pueden transformar negocios desde su base.</p><br />
                     <p className="Main-aboutText">Mi paso por Declarando Asesores fue clave para desarrollar una visión más analítica y orientada al cliente. Como SDR y Account Executive, aprendí a detectar necesidades reales, ofrecer soluciones y entender el valor de una experiencia digital funcional y bien diseñada. Fue allí donde nació mi interés por crear herramientas y no solo comercializarlas.</p><br />
-                    <p className="Main-aboutText">Ese impulso me llevó a formarme como desarrollador web, combinando mi perfil creativo con habilidades técnicas. En el CEI Valencia aprendí a construir aplicaciones completas, desde la idea y el diseño en Figma hasta el desarrollo en React, NextJS, Express o MongoDB. Hoy busco seguir creciendo en proyectos donde diseño, tecnología y experiencia de usuario se unan con propósito.</p>
+                    <p className="Main-aboutText">Ese impulso me llevó a formarme como desarrollador web, combinando mi perfil creativo con habilidades técnicas. En el CEI Valencia aprendí a construir aplicaciones completas, desde la idea y el diseño en Figma hasta el desarrollo en React, NextJS, Express o MongoDB.</p>
+                    <p className="Main-aboutText">Posteriormente, pasé al lado más técnico con mi experiencia en Producción Digital en la agencia WPP Production, donde trabajé en proyectos reales orientados a cliente, consolidando mi perfil entre negocio y tecnología.</p>
+                    <p className="Main-aboutText">En la actualidad, trabajo como Salesforce Marketing Cloud Developer en NATEEVO (grupo VASS) desarrollando soluciones digitales escalables, bien integradas y alineadas con objetivos de cliente en entornos exigentes.</p>
                 </div>
-                <p className="Main-aboutAction">Si has llegado hasta aquí, <a className="Link-action" href="mailto:jaime.nebot.colom@gmail.com" title="Enviar correo">¿por qué no hablamos? </a>🚀</p>
+                <p className="Main-aboutAction">Si has llegado hasta aquí y tienes alguna propuesta, <a className="Link-action" href="mailto:jaime.nebot.colom@gmail.com" title="Enviar correo">¿por qué no hablamos? </a>🚀</p>
 
             </main>
             <Footer />
